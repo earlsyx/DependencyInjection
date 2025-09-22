5d7c90cc57797df4c4d883382f255bca5359b956
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-09-23 04:51:13 +0800

    Add Dependency injection extension Add serviceCollection Add serviceProvider
    add App get required service
    
    run,
    
    in App,
    Run, get requestnumber
    use scope , createScope from serviceprovider
    handle endpoint b callling ,ServiceProvider
    gettingrequeiredservice of SomeEndPoint class
    
    SomeEndpoint inject serviceA : inherit form BaseClass
    Handle() doWork
    
    Service A inherit from baseclass
     - has DoWork() , Out message;
    
    Baseclass
    - readonly _indentTracker newing
    - overrinding ToString()
    - Out metehod arument msg, Get Indent
    
    IndentTracker
    - GetIndent() , count everytinh in stacktrice,
    format, return new format.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,67 @@
-﻿
+﻿using Microsoft.Extensions.DependencyInjection;
+
+IServiceCollection serviceCollection = new ServiceCollection();
+
+serviceCollection.AddSingleton<SomeEndPoint>();
+serviceCollection.AddSingleton<ServiceA>();
+serviceCollection.AddSingleton<App>();
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+
+var app = serviceProvider.GetRequiredService<App>();
+for (int i = 1; i < 4; i++)
+{
+    app.Run(i, serviceProvider);
+}
+public class App
+{
+    public void Run(int requestNumber, ServiceProvider serviceProvider)
+    {
+        Console.WriteLine($"Request {requestNumber}:");
+        using var scope = serviceProvider.CreateScope();
+        var endpoint = scope.ServiceProvider
+            .GetRequiredService<SomeEndPoint>();
+        endpoint.Handle();
+    }
+}
+
+public class SomeEndPoint(ServiceA serviceA) : BaseClass
+{
+    private readonly ServiceA _serviceA = serviceA;
+    public void Handle()
+    {
+        Out($"{this}SomeEndPoint.Handle");
+        _serviceA.DoWork();
+    }
+}
+
+public class ServiceA : BaseClass
+{
+    internal void DoWork()
+    {
+        Out($"{this} doing work.");
+    }
+}
+public class BaseClass
+{
+    private readonly IndentTracker _indentTracker = new();
+    public override string ToString()
+    {
+        return base.ToString() + '-' + GetHashCode().ToString()[^4..];
+    }
+
+    public void Out(string msg)
+    {
+        Console.WriteLine($"{_indentTracker.GetIndent()}{msg}");
+    }
+}
+
+public sealed class IndentTracker
+{
+    public string GetIndent()
+    {
+        int count = Environment.StackTrace.Count(a => a == '\n') - 5;
+
+        return new string(' ', count * 2);
+    }
+}
